3e739a5805d91ad79d525bd270ef4a835ad4fd67
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-01 21:33:14 +0100

    Blocker: doesnt find role list

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import './App.css';
 import EmployeeList from './components/EmployeeList'
 
 function App() {
-  return (
-    <>
-        <section name="header">
-            <h1>Employee list</h1>
-        </section>
-        
-        <section name="main">
-            <EmployeeList />
-          </section>
-    </>
-  );
+    return (
+        <>
+            <section name="header">
+                <h1>Employee list</h1>
+            </section>
+
+            <section name="main">
+                <EmployeeList />
+            </section>
+        </>
+    );
 }
 
 export default App;
--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -5,10 +5,22 @@ class EmployeeList extends Component {
     state = {
         employees: []
     }
+
+    componentDidMount() {
+        this.fetchData()
+    }
+    
+    fetchData = async () => {
+        let response = await axios.get('https://reqres.in/api/users?per_page=5')
+        this.setState({ employees: response.data.data })
+    }
+
+   
     
     render() {
         const { employees } = this.state;
-        const employeeList = employees.map(employee => {
+        let employeeList;
+        employeeList = employees.map(employee => {
             return (
                 <li key={employee.id}>
                     {`${employee.first_name} ${employee.last_name}`}
