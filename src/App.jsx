d6b4140e79267cdbba5bbdf7302a0aa51e80a280
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-02 10:21:06 +0100

    Employee list as props now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import './App.css';
+import React, { Component } from 'react'
+
 import EmployeeList from './components/EmployeeList'
+import InfoCard from './components/InfoCard'
 
-function App() {
-    return (
-        <>
-            <section name="header">
-                <h1>Employee list</h1>
-            </section>
+class App extends Component {
+    state = {
+        employees: []
+    }
 
-            <section name="main">
-                <EmployeeList />
-            </section>
-        </>
-    );
+    async componentDidMount() {
+        // let response = await axios.get('https://reqres.in/api/users?per_page=5')
+        let response = await (await fetch('https://reqres.in/api/users?per_page=5')).json()
+        this.setState({ employees: response.data})
+    }
+    
+    render() {
+        return (
+            <>
+                <section name="header">
+                    <h1>Employee list</h1>
+                </section>
+    
+                <section name="main">
+                    <EmployeeList employees={this.state.employees} />
+                    <InfoCard employees={this.state.employees} />
+                </section>
+            </>
+        );
+    }
 }
 
 export default App;
--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,34 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import axios from 'axios'
 
-class EmployeeList extends Component {
-    state = {
-        employees: []
-    }
 
-    async componentDidMount() {
-        // let response = await axios.get('https://reqres.in/api/users?per_page=5')
-        let response = await (await fetch('https://reqres.in/api/users?per_page=5')).json()
-        this.setState({ employees: response.data})
-    }
+const EmployeeList = (props) => {
     
-
-    render() {
-        const { employees } = this.state;
-        let employeeList;
-        employeeList = employees.map(employee => {
-            return (
-                <li key={employee.id}>
-                    {`${employee.first_name} ${employee.last_name}`}
-                </li>
-            )
-        });
-        
+    let employeeList;
+    employeeList = props.employees.map(employee => {
         return (
-            <ul role="list">
-                {employeeList}
-            </ul>
+            <li key={employee.id}>
+                {`${employee.first_name} ${employee.last_name}`}
+            </li>
         )
-    }
+    });
+    
+    return (
+        <ul role="list">
+            {employeeList}
+        </ul>
+    )
 }
 export default EmployeeList
