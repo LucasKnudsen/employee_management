e2656f594c0f4f687b16a7914975b8618a756b02
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-02 10:07:14 +0100

    Branching off to pass props directly from app

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+// Needs to inherit props.
+
+class InfoCard extends Component {
+    
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default InfoCard
